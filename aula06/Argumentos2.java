14764360d5b42c0bfd94e4ceb0c97ecdbda0d59b
reneaps <reneaps@ig.com.br>
2015-04-09 11:10:09 -0300

    Signed-off-by: reneaps <reneaps@ig.com.br>

--- aula06/Argumentos2.java
+++ aula06/Argumentos2.java
@@ -4,8 +4,11 @@ class Argumentos2 {
 	
 	public static void main(String[] args){
 		Calendar c = Calendar.getInstance();
-        int ano = c.get(Calendar.YEAR);
+		int dia = c.get(Calendar.DAY_OF_MONTH);
+		int mes = (c.get(Calendar.MONTH)+1);
+        	int ano = c.get(Calendar.YEAR);
 		System.out.println ("Ola Mundo! -> " + args[0]);
-		System.out.println("Ano: " + ano);
+		System.out.println("Ano: " + dia + "/" + mes + "/" + ano);
+		System.out.print("Date And Time Is: " + c.getTime());
 	}
 }
