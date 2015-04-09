8b93a8d821847bd6682946eb7e428f26224c31c1
reneaps <reneaps@ig.com.br>
2015-04-09 10:20:19 -0300

    Data
    Adicionado saida do ano

--- aula06/Argumentos2.java
+++ aula06/Argumentos2.java
@@ -1,7 +1,11 @@
+import java.util.Calendar;
+
 class Argumentos2 {
 	
 	public static void main(String[] args){
-		
+		Calendar c = Calendar.getInstance();
+        int ano = c.get(Calendar.YEAR);
 		System.out.println ("Ola Mundo! -> " + args[0]);
+		System.out.println("Ano: " + ano);
 	}
 }
